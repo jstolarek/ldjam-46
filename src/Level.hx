dae26b3546db653bc4e786c72d57be3e33a65c65
Jan Stolarek <jan.stolarek@ed.ac.uk>
2020-04-18 17:23:54 +0100

    Wstępne ładowanie poziomu

--- src/Level.hx
+++ src/Level.hx
@@ -5,11 +5,18 @@ class Level extends dn.Process {
     public var wid(get,never) : Int; inline function get_wid() return 16;
     public var hei(get,never) : Int; inline function get_hei() return 16;
 
+	var project : ogmo.Project;
+	var data : ogmo.Level;
+
     var invalidated = true;
 
     public function new() {
         super(Game.ME);
         createRootInLayers(Game.ME.scroller, Const.DP_BG);
+		project = new ogmo.Project(hxd.Res.load("map/park.ogmo"), false);
+		data = project.getLevelByName("park");
+
+        //JSTOLAREK: load collision map here if we want to support collisions
     }
 
     public inline function isValid(cx,cy) return cx>=0 && cx<wid && cy>=0 && cy<hei;
@@ -18,13 +25,15 @@ class Level extends dn.Process {
 
     public function render() {
         // Debug level render
-        root.removeChildren();
-        for(cx in 0...wid)
-        for(cy in 0...hei) {
-            var g = new h2d.Graphics(root);
-            g.beginFill(Color.randomColor(rnd(0,1), 0.5, 0.4), 1);
-            g.drawRect(cx*Const.GRID, cy*Const.GRID, Const.GRID, Const.GRID);
-        }
+
+      for(l in data.layersReversed)
+        switch l.name {
+            case "collisions","entities": // nope
+            case "front":
+              var e = l.render(root);
+              e.filter = new h2d.filter.Glow(0x0, 0.4, 32, 2, 2, true);
+              case _: l.render(root);
+          }
     }
 
     override function postUpdate() {
