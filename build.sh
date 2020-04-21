1b146d9626d3e0ca413f59e0a1bbc59d8581156e
Jan Stolarek <jan.stolarek@ed.ac.uk>
2020-04-21 22:25:29 +0100

    Poprawka w skryptach budujących

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Builds a native Linux version
-mkdir -p bin/stroll
+mkdir -p bin/stroll_src
 haxe hl.c.sdl.hxml > /dev/null && \
-clang -O3 -o bin/stroll -std=c17 -I bin/stroll bin/stroll/main.c /usr/lib/*.hdll -lhl -lSDL2 -lopenal -lm -lGL
+clang -O3 -o bin/stroll -std=c17 -I bin/stroll_src bin/stroll_src/main.c /usr/lib/*.hdll -lhl -lSDL2 -lopenal -lm -lGL
 
