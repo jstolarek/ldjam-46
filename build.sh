2d37d8df652d8db8799179cd531e9db5f9288416
Jan Stolarek <jan.stolarek@ed.ac.uk>
2020-04-23 18:04:22 +0100

    Skrypty budujące

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@
 mkdir -p bin/stroll_src
 haxe hl.c.sdl.hxml > /dev/null && \
 clang -O3 -o bin/stroll -std=c17 -I bin/stroll_src bin/stroll_src/main.c /usr/lib/*.hdll -lhl -lSDL2 -lopenal -lm -lGL
-
--- init.sh
+++ init.sh
@@ -14,5 +14,6 @@ haxelib dev deepnightLibs deepnightLibs/
 haxelib dev castle castle/
 haxelib dev heapsOgmo heapsOgmo/
 
-haxelib install hlsdl     # hldx needed on Windows
+haxelib install hlsdl
+haxelib install hldx      # for cross-compiling Windows binaries
 haxelib install hashlink  # needed for native C builds
